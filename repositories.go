145fd393a153d260db33a74094c624a4598cf7a3
Zeph Grunschlag <zgrunschlag@gmail.com>
2017-03-30 12:41:22 -0400

    hacky pagination for Repositories ... WIP

--- repositories.go
+++ repositories.go
@@ -1,7 +1,7 @@
 package bitbucket
 
 import (
-//"github.com/k0kubun/pp"
+	"fmt"
 )
 
 type Repositories struct {
@@ -23,12 +23,28 @@ func (r *Repositories) ListForAccount(ro *RepositoriesOptions) interface{} {
 	return r.c.execute("GET", url, "")
 }
 
-func (r *Repositories) ListForTeam(ro *RepositoriesOptions) interface{} {
-	url := r.c.requestUrl("/repositories/%s", ro.Owner)
-	if ro.Role != "" {
-		url += "?role=" + ro.Role
+func (r *Repositories) ListForTeam(ro *RepositoriesOptions, pages ...uint) interface{} {
+	fmt.Println("Inside ListForTeam")
+	fmt.Printf("pages = %v\n", pages)
+	if(pages == nil){
+		pages = []uint{1}
 	}
-	return r.c.execute("GET", url, "")
+	if(len(pages) == 1) {
+		fmt.Printf("pages = %v\n", pages)
+		url := r.c.requestUrl("/repositories/%s?page=%d", ro.Owner, pages[0])
+		if ro.Role != "" {
+			url += "?role=" + ro.Role
+		}
+		return r.c.execute("GET", url, "")
+	} else {
+		first := pages[0]
+		last := pages[1]-1
+		result := make([]interface{}, last-first+1)
+		for page := first; page <= last; page++ {
+			result[page-first] = r.ListForTeam(ro, []uint{page}...)
+		}
+		return result
+	}
 }
 
 func (r *Repositories) ListPublic() interface{} {
